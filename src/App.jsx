eddebc17377982e06c40c54f76eebdaf797ce068
edthuman <edwardadirvine@aol.com>
2024-04-23 15:21:22 +0100

    add Home component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css'
+import Home from './Home/Home'
 
 function App() {
   return (
-    <>
-      <h1>Welcome to my portfolio website!</h1>
-    </>
+    <main>
+      <Home/>
+    </main>
   )
 }
 
---
+++ src/Home/Home.jsx
@@ -0,0 +1,24 @@
+import "./Home.css"
+
+function Home () {
+    return <>
+        <section id="links">
+            <a className="social-link" href="https://github.com/edthuman" target="blank">
+                <img src="github-logo.png" alt="GitHub logo" className="social-logo"/>
+            </a>
+            <a className="social-link" href="https://www.linkedin.com/in/edwardirvine" target="blank" id="linkedin-link">
+                <img src="linkedin-logo.png" alt="LinkedIn logo" className="social-logo" />
+            </a>
+        </section>
+        <section id="home">
+            <h1>Hi, I'm Ed</h1>
+            <p className="home-text" id="software-dev-text">Junior Software Developer</p>
+        </section>
+        <footer id="home-footer">
+                <p className="home-text" id ="scroll-down-text">Scroll down to learn more and see what I've made!</p>
+                <p className="home-text" id="down-arrow">↓</p>
+        </footer>
+    </>
+}
+
+export default Home
