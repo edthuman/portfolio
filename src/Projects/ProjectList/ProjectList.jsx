c6b04cefff8b15cf9002604b7497e341236be126
edthuman <edwardadirvine@aol.com>
2024-04-27 18:17:21 +0100

    add scrolling and scroll keys to ProjectList

--- src/Projects/ProjectList/ProjectList.jsx
+++ src/Projects/ProjectList/ProjectList.jsx
@@ -1,12 +1,27 @@
-import ProjectCard from "./PorfolioCard"
+import ProjectCard from "./ProjectCard"
 import * as data from "./data/project-details.json"
 import "./ProjectList.css"
+import { useRef } from "react";
 
 function ProjectList() {
     const projectDetails = data.projects
+    const scrollBar = useRef();
 
-    return <section id="projects-list">
-        {projectDetails.map((project) => <ProjectCard project={project} key={project.name}/>)}
+    function handleScroll(scrollAmount) {
+        const newScrollPosition = scrollBar.current.scrollLeft + scrollAmount;
+
+        if((newScrollPosition + 200) >= 0 && newScrollPosition <= (scrollBar.current.scrollLeftMax + 200)){ 
+            scrollBar.current.scrollLeft = newScrollPosition;
+        }
+    };
+
+    return <section id="projects-buttons">
+        <button className="scroll-button" onClick={() => handleScroll(-200)}>{"<"}</button>
+        <section id="projects-list" ref={scrollBar}>
+            {projectDetails.map((project) => <ProjectCard project={project} key={project.name}/>)}
+        </section>
+        <button className="scroll-button" onClick={() => handleScroll(200
+        )}>{">"}</button>
     </section>
 }
 
