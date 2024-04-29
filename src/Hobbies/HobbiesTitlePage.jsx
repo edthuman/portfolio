85aaea3dbab5901801c6528ddc37cbf77cb71552
edthuman <edwardadirvine@aol.com>
2024-04-29 18:05:44 +0100

    add HobbiesTitlePage component

---
+++ src/Hobbies/HobbiesTitlePage.jsx
@@ -0,0 +1,10 @@
+import "./HobbiesTitlePage.css"
+
+function HobbiesTitlePage() {
+    return <section id="hobbies-title-page" className="hobbies-sub-page">
+    <h2 id="hobby-title-page-heading">Hobbies</h2>
+    <p id="hobby-title-page-subheading">Scroll to see what I get up to in my free time {"->"}</p>
+</section>
+}
+
+export default HobbiesTitlePage
