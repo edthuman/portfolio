9920e62389a480042c1682e343295f4eabcdb5e4
edthuman <edwardadirvine@aol.com>
2024-04-29 18:06:45 +0100

    add MusicPage component

---
+++ src/Hobbies/MusicPage.jsx
@@ -0,0 +1,13 @@
+import "./MusicPage.css"
+
+function MusicPage() {
+    return <section className="hobbies-sub-page" id="music-page">
+        <h2 className="hobby-heading">Music</h2>
+        <p className="hobby-page-text">I love to discover new songs, artists, or genres.</p>
+        <p className="hobby-page-text">An especially good source for obscure music has been the app <a href="https://musicboard.app/" target="blank" className="page-link">MusicBoard</a>, which I'm also using to keep track of what I've listened to.</p>
+        <p className="hobby-page-text">You can view a list of every album I've ever listened to on my <a href="https://musicboard.app/edthuman/albums" target="blank" className="page-link">profile</a>. In 2023, I listened to 600 new albums!</p>
+        
+    </section>
+}
+
+export default MusicPage
