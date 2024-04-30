beb1699f37fc5a0c11db2f583885c99dc7151f45
edthuman <edwardadirvine@aol.com>
2024-04-30 16:18:02 +0100

    update subtitle of Technologies

--- src/Technologies/Technologies.jsx
+++ src/Technologies/Technologies.jsx
@@ -4,7 +4,7 @@ import TechnologyGrid from "./TechnologyGrid/TechnologyGrid"
 function Technologies() {
     return <section id="technologies" className="page" tabIndex={0}>
         <h2 id="technologies-heading">Technologies</h2>
-        <p className="subheading" id="technologies-subheading">The technologies I have experience with</p>
+        <p className="subheading" id="technologies-subheading">The tech I have experience using</p>
         <TechnologyGrid/>
     </section>
 }
