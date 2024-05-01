d55a05dcde55e3f4ea69edc7c74e35a81b22e457
edthuman <edwardadirvine@aol.com>
2024-05-01 15:52:16 +0100

    update Projects text, class, and id

--- src/Projects/Projects.jsx
+++ src/Projects/Projects.jsx
@@ -3,9 +3,9 @@ import "./Projects.css"
 
 function Projects() {
     return <section id="projects" className="page" tabIndex={0}>
-            <h2>Projects</h2>
-            <p className="subheading">
-                Click on a project preview to read more
+            <h2 id="projects-heading">Projects</h2>
+            <p className="subheading" id="projects-subheading">
+                Scroll across to see any hidden projects, or click on a project to read more about it
             </p>
             <ProjectList/>
         </section>
